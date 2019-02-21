860a768bcdcbbc520b6498efbe5d508b1959e9a6
valeria-ivasheva <valeria.ivasheva@outlook.com>
2019-02-22 01:36:42 +0300

    added 1.3 task

---
+++ F#/ListRevers/ListRevers/Program.fs
@@ -0,0 +1,14 @@
+﻿let reverse list = 
+    if (List.length list = 0) then []
+    else
+        let rec rev lst accList = 
+            if (List.length lst = 0) then accList
+            else
+                rev (List.tail lst) ((List.head lst) :: accList)
+        rev list []
+
+let list = reverse [1; 2; 3 ; 4; 5; 6]
+printf "%A" list
+
+
+
