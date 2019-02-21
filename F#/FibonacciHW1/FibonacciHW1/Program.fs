ad9afc7e3c78942d3b7e1e771f08c5a1736e74d6
valeria-ivasheva <valeria.ivasheva@outlook.com>
2019-02-21 23:02:19 +0300

    Added 1.2

---
+++ F#/FibonacciHW1/FibonacciHW1/Program.fs
@@ -0,0 +1,15 @@
+﻿let Fibonacci n = 
+    if n <= 0 then 
+        -1
+    else 
+        let rec fib x temp acc = 
+            if x <= 2 then acc
+            else 
+                fib (x - 1) acc (acc + temp)
+        fib n 1 1
+
+[<EntryPoint>]
+let main argv =
+    let f = Fibonacci 22
+    printf "Фибоначчи номер 22 %d" f
+    0
