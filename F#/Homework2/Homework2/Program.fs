844195b077fd78f87f14029773b0b66ea1b18a73
valeria-ivasheva <valeria.ivasheva@outlook.com>
2019-02-25 14:15:04 +0300

    Убрала лишнее

--- F#/Homework2/Homework2/Program.fs
+++ F#/Homework2/Homework2/Program.fs
@@ -1,10 +1,8 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 
 [<EntryPoint>]
 let main argv =
     let str = "a n N a"
     let res = Palindrome.isPalindrome str
     printf "%A" (res)
-    0 // return an integer exit code
+    0 
