9e63480887bb3cc0029ed0dfb59e0dadffaa17a2
valeria-ivasheva <valeria.ivasheva@outlook.com>
2019-04-06 21:38:07 +0300

    Added Homework5 (1 and 2)

---
+++ F#/Homework5/Homework5/Brackets.fs
@@ -0,0 +1,36 @@
+﻿module Brackets
+
+let returnOpenBrackets brk = 
+    match brk with 
+    |')' -> Some('(')
+    |'}' -> Some('{')
+    |']' -> Some('[')
+    | _  -> None
+
+let isCorrectBrackets str = 
+    let listStr = Seq.toList str
+    let rec correct listStr (stack) = 
+        let closeBcktFun bct =
+            if (List.isEmpty stack) then false
+            else
+                (List.head stack) = (returnOpenBrackets bct).Value
+        let openBcktFun bct = 
+            let stack = bct :: stack
+            correct (List.tail listStr) stack
+        if (List.isEmpty listStr)
+        then List.isEmpty stack
+        elif (List.head listStr = '{' ||  List.head listStr = '(' || List.head listStr = '[')
+        then 
+            openBcktFun (List.head listStr)
+        elif (List.head listStr = '}' || List.head listStr = ')' || List.head listStr = ']')
+        then 
+            let result = closeBcktFun (List.head listStr)
+            if (result) 
+            then correct listStr.Tail stack.Tail
+            else false
+        else
+            correct listStr.Tail stack
+    correct listStr []
+
+
+        
---
+++ F#/Homework5/Homework5/Task2.fs
@@ -0,0 +1,9 @@
+﻿///Записать в point-free стиле func x l = List.map (fun y -> y * x) l
+module Task2
+
+let func x l = List.map (fun y -> y * x) l
+
+let func1 x = List.map (fun y -> y * x)
+
+let func2 = List.map << (*) 
+
