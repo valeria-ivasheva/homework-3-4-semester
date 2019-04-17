ff7be52ecdfad64d859c417ed6134b0b5053d9a8
valeria-ivasheva <valeria.ivasheva@outlook.com>
2019-04-17 14:00:31 +0300

    Тесты на задачу 5.3

---
+++ F#/Homework5/HomeworkTests/TelephoneTest.fs
@@ -0,0 +1,21 @@
+﻿module TelephoneTest
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Telephone
+open FsUnit
+
+[<TestClass>]
+type TestClass () =
+    let list = [("Olya", "12-12-12"); ("Vitya", "21-43-90"); ("Sveta", "43-56-94"); ("Roma", "04-43-78"); ("Slava","85-45-32"); ("Olya","23-44-76")]
+
+    [<TestMethod>]
+    member this.``Telephone findPhone correct work`` () = 
+        findPhone "Olya" list |> should equal ["12-12-12"; "23-44-76"]
+
+
+    [<TestMethod>]
+    member this.``Telephone findName correct work`` () = 
+        Telephone.findName "12-12-12" list |> should equal ["Olya"]
+        Telephone.findName "43-56-94" list |> should equal ["Sveta"]
+
+    
